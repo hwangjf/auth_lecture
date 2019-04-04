a1e9f30796d28fc37b24cf8bc88f63cd0f3b00d7
hwangjf <jeffreyhwang90@gmail.com>
2019-04-04 09:12:33 -0400

    test

--- eBots_API/app/controllers/api/v1/auth_controller.rb
+++ eBots_API/app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::AuthController < ApplicationController
+  def login
+    user = User.find_by(username: params[:username])
+
+    if user && user.authorize(params[:password])
+      render json: user
+    else
+      render json: {errors: "Please provide the correct username and password!"}
+    end
+  end
+
+  def auto_login
+    user_id = request.headers["Authorization"]["user_id"]
+    user = User.find(user_id)
+
+    if user
+      render json: user
+    else
+      render json: {errors: "yo... try again mister"}
+    end
+  end
 end
--- eBots_API/app/controllers/application_controller.rb
+++ eBots_API/app/controllers/application_controller.rb
@@ -1,2 +1,45 @@
 class ApplicationController < ActionController::API
+
+  def auth_header
+    request.headers["Authorization"]
+    # Bearer <JWT>
+  end
+
+  def encode_token(user_id)
+    JWT.encode({user_id: user_id}, 'secret')
+    # returns the hashed json web token or jwt
+  end
+
+  def decoded_token
+    if auth_header
+      begin  
+        # JWT.deocde(auth_headers, 'secret')
+        # decode the authorization key in headers
+        # JWT.deocde(auth_headers, 'secret') => [{ "user_id"=> "2"}, {"alg"=> "HS256"}]
+        # to send back the user_id
+        JWT.decode(auth_headers, 'secret')[0]["user_id"]
+      rescue
+        nil
+      end
+    end
+  end
+
+  def curr_user
+    if decoded_token
+      User.find(decoded_token)
+    end
+    # the else is simply to return nil which is a falsey value
+    # to be more explicit send back false
+  end
+
+  def logged_in
+    !!curr_user
+  end
+
+  def authorized
+    # if we want to protect many routes or large parts of your application 
+    # make this a before action at this level and skip where necessary otherwise skip this part
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+
 end
